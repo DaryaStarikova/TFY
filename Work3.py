b0a92b0b8283d35ed85ae0026b34da1c39345d01
DaryaStarikova <143960163+DaryaStarikova@users.noreply.github.com>
2023-12-05 01:05:42 +0300

    Update Work3.py

--- Work3.py
+++ Work3.py
@@ -1,7 +1,9 @@
 import string
+
 alphabetLC = list(string.ascii_lowercase)
 alphabetUC = list(string.ascii_uppercase)
 delims = ['>', '<', ':', '=', ',', ';']
+
 class Lexer:
     def __init__(self, input_string):
         self.input_string = input_string
@@ -11,7 +13,7 @@ class Lexer:
 
     def is_number(self, value):
         try:
-            float(value)
+            float(value.rstrip('FfLl'))
             return True
         except ValueError:
             return False
@@ -21,36 +23,30 @@ class Lexer:
         self.current_token = ''
 
     def analyze(self):
-        res = input_string.split()
+        res = self.input_string.split()
         for i in res:
             if i in ('if', 'else', 'for', 'while', 'do'):
                 self.current_token = i
                 self.add_token('KEYWORD', self.current_token)
-                # self.current_state = 'KEYWORD'
-            elif i in (alphabetLC) or i in (alphabetUC):
+            elif i in alphabetLC or i in alphabetUC:
                 self.current_token = i
                 self.add_token('ID', self.current_token)
-                # self.current_state = 'ID'
             elif self.is_number(i):
                 self.current_token = i
                 self.add_token('NM', self.current_token)
-                # self.current_state = 'NM'
-            elif i == (':='):
+            elif i == ':=':
                 self.current_token = i
                 self.add_token('ASGN', self.current_token)
-                # self.current_state = 'ASGN'
-            elif i in (delims):
+            elif i in delims:
                 self.current_token = i
                 self.add_token('DLM', self.current_token)
-                # self.current_state = 'DLM'
             else:
                 self.current_token = i
                 self.add_token('ERR', self.current_token)
-                # self.current_state = 'ERR'
         return self.tokens
 
 # Пример использования:
-input_string = "if x > 0 : y := 3.14 ; else : z := 2"
+input_string = "if x > 0 : y := 3.14 ; else : z := 2.5F"
 lexer = Lexer(input_string)
 tokens = lexer.analyze()
 for token in tokens:
