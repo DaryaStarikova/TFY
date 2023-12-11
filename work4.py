648bee83b96bce487d4af7d2a4d1e84109057e9b
DaryaStarikova <143960163+DaryaStarikova@users.noreply.github.com>
2023-12-11 21:13:04 +0300

    Update work4.py

--- work4.py
+++ work4.py
@@ -1,48 +1,51 @@
 # вычисляет эпсилон-замыкание набора состояний в NFA (добавляет состояния, достижимые посредством эпсилон-переходов)
 def epsilon_closure(states, transitions, epsilon='eps'):
-    closure = set(states) #начальный набор состояний
-    stack = list(states) #стек инициализируется начальным набором
+    closure = set(states)  # начальный набор состояний
+    stack = list(states)  # стек инициализируется начальным набором
     while stack:
-        state = stack.pop() #извлекаем состояние из стека
-        #цикл переходов
+        state = stack.pop()  # извлекаем состояние из стека
+        # перебор всех переходов NFA
         for transition in transitions:
-            #соответствует ли текущий переход рассматриваемому состоянию и использует ли переход эпсилон в качестве входных данных
+            # если есть эпсилон-переход из текущего состояния
             if transition[0] == state and transition[1] == epsilon:
-                #проверяет, не находится ли следующее состояние эпсилон-перехода в наборе замыканий
+                # если состояние после эпсилон-перехода не в замыкании
                 if transition[2] not in closure:
                     closure.add(transition[2])
                     stack.append(transition[2])
     return closure
 
-# вычисляет набор состояний, достижимых из начальных состояний, используя определенный входной символ
+
+# вычисляет множество состояний, в которые можно перейти из заданного множества states по заданному символу symbol
 def move(states, transitions, symbol):
-    result = set()
+    result = set()  # пустое множество, в которое будут добавляться состояния после переходов
     for state in states:
         for transition in transitions:
             if transition[0] == state and transition[1] == symbol:
                 result.add(transition[2])
     return result
 
+
 # выполняет преобразование NFA в DFA с использованием метода построения подмножества
 def nfa_to_dfa(states, alphabet, transitions, initial_states, final_states):
-    dfa_states = [] # инициализация состояний DFA
-    dfa_transitions = [] # переходы
-    dfa_initial_state = frozenset(epsilon_closure(initial_states, transitions)) # начальное состояние DFA
-    dfa_states.append(dfa_initial_state) #добавляется начальное состояние DFA
-    stack = [dfa_initial_state] #создается стек и инициализируется начальное состояние DFA
+    dfa_states = []  # список состояний DFA
+    dfa_transitions = []  # список переходов DFA
+    dfa_initial_state = frozenset(epsilon_closure(initial_states, transitions))  # начальное состояние DFA
+    dfa_states.append(dfa_initial_state)
+    stack = [dfa_initial_state]
 
     while stack:
         current_dfa_state = stack.pop()
+        # перебор символов в алфавите
         for symbol in alphabet:
-            #Рассчитывается следующее количество состояний в NFA, достижимое от текущего состояния DFA по символам в алфавите
+            # переход к новому состоянию с учетом эпсилон-замыкания
             next_nfa_state = epsilon_closure(move(current_dfa_state, transitions, symbol), transitions)
-            #Если новое состояние next_nfa_stateне встречается во множестве изменений DFA
+            # если новое состояние не встречается во множестве состояний DFA
             if next_nfa_state not in dfa_states:
                 dfa_states.append(next_nfa_state)
                 stack.append(next_nfa_state)
-            #Добавляет переход во множество переходов DFA
+            # добавляем переход во множество переходов DFA
             dfa_transitions.append((current_dfa_state, symbol, next_nfa_state))
-    #Рассчитывается множество конечных результатов DFA
+    # рассчитываем множество конечных результатов DFA
     dfa_final_states = [state for state in dfa_states if any(s in state for s in final_states)]
 
     return dfa_states, alphabet, dfa_transitions, dfa_initial_state, dfa_final_states
@@ -59,7 +62,7 @@ def main():
     dfa_states, dfa_alphabet, dfa_transitions, dfa_initial_state, dfa_final_states = nfa_to_dfa(
         states, alphabet, transitions, initial_states, final_states)
 
-    # Преобразование набороров состояний в ожидаемый формат
+    # Преобразование набороров состояний в строковый вид
     dfa_states_str = [''.join(sorted(state)) for state in dfa_states]
     dfa_final_states_str = [''.join(sorted(state)) for state in dfa_final_states]
 
@@ -74,6 +77,7 @@ def main():
     initial_state_str = ''.join(sorted(dfa_initial_state))
     print("Initial states: " + initial_state_str)
     print("Final states: " + ', '.join(dfa_final_states_str))
+
 
 if __name__ == "__main__":
     main()
